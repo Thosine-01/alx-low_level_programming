62c6b8030a5f89c481bba854372d54b74006dd5d
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-09-12 12:18:42 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print a hexadecimal string
+ *
+ * Return: Always (SUccess)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
