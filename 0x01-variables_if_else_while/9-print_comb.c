2852c3fa60689bd0b63ebdbe9a9ed1b927ac28b3
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-09-12 13:39:08 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,24 +3,24 @@
 /**
  * main - Prints a series of numbers with commas
  *
- * Return: Always (Success)
+ * Return: Alway (Success)
  */
 int main(void)
 {
 	int c;
 
-	 for (c = '0'; c <= '9'; c++)
-	 {
-		 putchar(c);
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
 
-		 if (c != '9')
-		 {
-			 putchar(',');
-			 putchar(' ');
-		 }
-	 }
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-	 putchar('\n');
+	putchar('\n');
 
-	 return (0);
+	return (0);
 }
