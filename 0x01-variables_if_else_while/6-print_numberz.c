22fa791ce7ea3ffd44273eec8957afcda1c76d70
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-09-12 10:36:40 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print the numbers since 0 to 9
+ *
+ * Return: Always (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
