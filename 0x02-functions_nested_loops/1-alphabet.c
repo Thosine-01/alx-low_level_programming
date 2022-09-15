98346a722688c465f47e3f868f49abc404a626df
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-09-14 23:40:35 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- *  * main - write function to print lowercase alphabet
- *   * 
- *    * Return: 0
- *     */
+ * main - write function to print lowercase alphabet
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    void print_alphabet(void)
-    {
-            char c;
-            
-            for (c = 'a'; c <= 'z'; c++)
-            {
-	                putchar(c);
-	            }
-            
-            putchar('\n');
-        }
-    
-    print_alphabet();
-    
-    return (0);
-}}
+	void print_alphabet(void)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+
+		putchar('\n');
+	}
+
+	print_alphabet();
+
+	return (0);
+}
