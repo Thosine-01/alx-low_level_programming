181a66fec07e92d54f14f6f02fd59ba46aacd035
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-09-15 15:40:50 -0700

    task6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 	{
 		for (x = 0; x <= 14; x++)
 		{
-			if (x > 9)
-			
+			if (x > 9)	
 			_putchar((x / 10) + '0');
 			_putchar((x % 10) + '0');
 		}
+
 		_putchar('\n');
 	}
 }
