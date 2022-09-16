7e83a555e2b49ec0076ce766a7f46f3027a1e096
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-09-15 17:18:06 -0700

    task12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number -prints numbers
+ * @n:number to be printed
+ * Return:void
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
+
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	_putchar (num % 10 + '0');
+}
