6d26df06e6f31601ae7f6822c6458edf9f1b5dee
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-09-26 02:24:48 -0700

    c-programming on pointers and arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * strncpy - copying a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
