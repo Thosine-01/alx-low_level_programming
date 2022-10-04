0bb28a8cf9f2bd40b9f4340215476dea791e2972
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-10-04 03:50:12 -0700

    task o

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int index;
 
-	arry = malloc(sizeof(char) * size);
+	array = malloc(sizeof(char) * size);
 
 	if (size == 0 || array == NULL)
 		return (NULL);
