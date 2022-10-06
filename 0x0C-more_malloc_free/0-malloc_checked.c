ae6713a13768858dcdb807d243e71d1d915b78c3
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-10-06 14:56:54 -0700

    update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * malloc_checked - allocates memory using malloc
  * @b: memory to allocae
  * Return: pointer to allocate memory or normal process terminate if error
  */
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
 	void *p;
 
