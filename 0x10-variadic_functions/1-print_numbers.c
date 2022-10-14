5029704d92aa74435c7dc900805b992d43ce388a
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-10-13 18:51:46 -0700

    task2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,7 +9,7 @@
  *
  * Return: void
  */
-void print_numbers(const char *separator, cost unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list numbers;
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,7 +6,6 @@
  * print_strings - prints strings followed by a new line
  * @separator: string to be printed between strings
  * @n: number of strings
- *
  * Return: void
  */
 void print_strings(const char *separator, const unsigned int n, ...)
