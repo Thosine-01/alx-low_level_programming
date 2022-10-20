1d990ac32dcfbc0f7f661f2346cd1301669f14a2
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-10-20 10:29:01 -0700

    update 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_list - print all the element of a list_t list
+ * print_list - prints all the elements of a list_t list
  * @h: singly linked list
  * Return: number of element in the list
  */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
 	size_t nelem;
 
