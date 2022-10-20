a155831f1934d049f54b6e4b3908adce9f855bb3
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-10-20 05:29:42 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,8 +7,9 @@
  */
 size_t print_list(const list_t *h);
 {
-	size_t nodes = 0;
+	size_t nelem;
 
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -16,7 +17,7 @@ size_t print_list(const list_t *h);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nodes++;
+		nelem++;
 	}
-	return (nodes);
+	return (nelem);
 }
