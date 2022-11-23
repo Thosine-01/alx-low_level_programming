294f41400da86d324b5faf855bec57a7ca9c6c11
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-11-22 17:48:14 -0800

    latest update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -11,5 +11,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+void _putchar(char c);
 
 #endif
