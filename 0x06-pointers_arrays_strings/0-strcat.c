dbdf92f86470acf15464dfcae0cfda752f7ab0b9
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-11-23 14:39:47 -0800

    pointers and array project

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
 /**
- * _strcat - concatenates two string
+ * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- * Return: Always 0.
+ *
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-
 	j = 0;
 	while (src[j] != '\0')
 	{
