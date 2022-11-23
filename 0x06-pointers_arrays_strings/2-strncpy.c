1d54c61191866861c3a36a26843d77d2bbed6e29
Lawal Oluwatomisin <lawaltosine01@gmail.com>
2022-11-23 15:49:47 -0800

    update on the array and pointer project

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,22 @@
 #include "main.h"
-
 /**
- * strncpy - copying a string
+ * _strncpy - copy a string
  * @dest: input value
  * @src: input value
  * @n: input value
- * Return: Always 0.
+ *
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
 	j = 0;
-
 	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
 		j++;
 	}
-
 	while (j < n)
 	{
 		dest[j] = '\0';
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 /**
  * leet - encode into 1337speak
  * @n: input value
